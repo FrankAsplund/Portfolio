85f055f61a6394c8698d01de44defeaeedfc7aaf
FrankAsplund <frank_asplund@hotmail.com>
2023-06-11 15:35:30 +0200

    Skills + home (WIP)

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 import Grid from "@mui/material/Unstable_Grid2";
@@ -9,152 +16,129 @@ import Box from "@mui/material/Box";
 export default function Skills() {
   return (
     <div>
-      <Box gridColumn="span 4">
-        <Grid
-          container
-          spacing={4}
-          minHeight={160}
-          columns={8}
-          sx={{
-            "--Grid-borderWidth": "1px",
-            borderTop: "var(--Grid-borderWidth) solid",
-            borderLeft: "var(--Grid-borderWidth) solid",
-            borderColor: "divider",
-            "& > div": {
-              borderRight: "var(--Grid-borderWidth) solid",
-              borderBottom: "var(--Grid-borderWidth) solid",
-              borderColor: "divider",
-            },
-          }}
-        >
-          <Grid item p={2} m={2}>
-            <Image
-              height={75}
-              width={75}
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
-              alt="React"
-            />
+      <Paper
+        sx={{
+          p: 2,
+          margin: 2,
+
+          flexGrow: 1,
+        }}
+      >
+        <Box gridColumn="span 4">
+          <Grid
+            container
+            rowSpacing={1}
+            spacing={4}
+            minHeight={160}
+            columns={4}
+            justifyContent="center"
+            alignItems="center"
+          >
+            {skillsData.map((skill) => (
+              <Grid item spacing={4} p={2} m={2} key={skill.id}>
+                <Image height={75} width={75} src={skill.src} alt={skill.alt} />
+                <Typography variant="h6">{skill.alt}</Typography>
+              </Grid>
+            ))}
           </Grid>
-
-          <Grid item p={2} m={2}>
-            <Image
-              height={75}
-              width={75}
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original-wordmark.svg"
-              alt="Vue"
-            />
-          </Grid>
-
-          <Grid item p={2} m={2}>
-            <Image
-              height={75}
-              width={75}
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
-              alt="JavaScript"
-            />
-          </Grid>
-
-          <Grid item p={2} m={2}>
-            <Image
-              height={75}
-              width={75}
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg"
-              alt="HTML5"
-            />
-          </Grid>
-
-          <Image
-            height={75}
-            width={75}
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg"
-            alt="CSS3"
-          />
-
-          <Image
-            height={75}
-            width={75}
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
-            alt="Next.js"
-          />
-
-          <Image
-            height={75}
-            width={75}
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuetify/vuetify-original.svg"
-            alt="Vuetify"
-          />
-
-          <Image
-            height={75}
-            width={75}
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
-            alt="Node.js"
-          />
-
-          <Image
-            height={75}
-            width={75}
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-            alt="Github"
-          />
-
-          <Image
-            height={75}
-            width={75}
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
-            alt="Git"
-          />
-
-          <Image
-            height={75}
-            width={75}
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
-            alt="Bootstrap"
-          />
-
-          <Image
-            height={75}
-            width={75}
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"
-            alt="Figma"
-          />
-
-          <Image
-            height={75}
-            width={75}
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg"
-            alt="Material UI"
-          />
-
-          <Image
-            height={75}
-            width={75}
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
-            alt="Docker"
-          />
-
-          <Image
-            height={75}
-            width={75}
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg"
-            alt="SASS"
-          />
-
-          <Image
-            height={75}
-            width={75}
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-original.svg"
-            alt="Wordpress"
-          />
-
-          <Image
-            height={75}
-            width={75}
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
-            alt="MySQL"
-          />
-        </Grid>
-      </Box>
+        </Box>
+      </Paper>
     </div>
   );
 }
+
+const skillsData = [
+  {
+    id: 1,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg",
+    alt: "React",
+  },
+  {
+    id: 2,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original-wordmark.svg",
+    alt: "Vue",
+  },
+  {
+    id: 3,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    alt: "JavaScript",
+  },
+  {
+    id: 4,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg",
+    alt: "HTML5",
+  },
+  {
+    id: 5,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg",
+    alt: "CSS3",
+  },
+  {
+    id: 6,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+    alt: "Next.js",
+  },
+  {
+    id: 7,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuetify/vuetify-original.svg",
+    alt: "Vuetify",
+  },
+  {
+    id: 8,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+    alt: "Node.js",
+  },
+  {
+    id: 9,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+    alt: "Github",
+  },
+  {
+    id: 10,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+    alt: "Git",
+  },
+  {
+    id: 11,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+    alt: "Bootstrap",
+  },
+  {
+    id: 12,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+    alt: "Figma",
+  },
+  {
+    id: 13,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+    alt: "Material UI",
+  },
+  {
+    id: 14,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+    alt: "Docker",
+  },
+  {
+    id: 15,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
+    alt: "SASS",
+  },
+  {
+    id: 16,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-original.svg",
+    alt: "Wordpress",
+  },
+  {
+    id: 17,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+    alt: "MySQL",
+  },
+];
+
+<style jsx>
+  {`
+    main {
+      font-family: Roboto;
+    }
+  `}
+</style>;
--- pages/index.jsx
+++ pages/index.jsx
@@ -14,11 +14,13 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+import Paper from "@mui/material/Paper";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 import Grid from "@mui/material/Unstable_Grid2";
 import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
 
 export default function Home() {
   return (
@@ -30,27 +32,65 @@ export default function Home() {
       <Navbar />
 
       <main>
-        <Stack direction="row" spacing={2}>
-          <Avatar
-            src="/images/pfp.jpg"
-            sx={{ width: 300, height: 300 }}
-            alt="Frank Asplund"
-          />
-        </Stack>
-        <h1>Frank Asplund</h1>
-        <h3>Front-end developer</h3>
-        <h4>
-          Dedicated and determined to deliver responsive, functional websites on
-          time.{" "}
-        </h4>
+        <Paper
+          elevation={2}
+          sx={{
+            p: 2,
+            m: 8,
+          }}
+        >
+          <Stack direction="row" spacing={8}>
+            <Avatar
+              src="/images/pfp.jpg"
+              sx={{ width: 300, height: 300 }}
+              alt="Frank Asplund"
+            />
 
-        <Grid>
-          <Image
-            height={50}
-            width={50}
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg"
-          />
-        </Grid>
+            <Stack useFlexGap flexWrap="wrap">
+              <Typography gutterBottom variant="h1" m={0}>
+                Frank Asplund
+              </Typography>
+              <Typography gutterBottom variant="h3">
+                Front-end developer
+              </Typography>
+              <Typography gutterBottom variant="h5">
+                Dedicated and determined to deliver responsive, functional
+                websites on time.{" "}
+              </Typography>
+            </Stack>
+          </Stack>
+
+          {/* <Paper
+            elevation={24}
+            sx={{
+              p: 0,
+              m: 2,
+            }}
+          > */}
+          <Grid item container rowSpacing={4} px={8} m={2}>
+            <Link
+              href="https://www.linkedin.com/in/frank-asplund-794187221/"
+              underline="none"
+            >
+              <Image
+                height={50}
+                width={50}
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg"
+                alt="LinkedIn"
+              />
+            </Link>
+
+            <Link href="https://github.com/FrankAsplund" underline="none">
+              <Image
+                height={50}
+                width={50}
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                alt="Github"
+              />
+            </Link>
+          </Grid>
+        </Paper>
+        {/* </Paper> */}
 
         <h1>My skills</h1>
         <Skills />
@@ -80,51 +120,18 @@ export default function Home() {
       <style jsx>{`
         main {
           font-family: Roboto;
-          padding: 5rem 0;
+          padding: 2rem 0;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
-        }
-        .container {
-          display: flex;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
       `}</style>
 
       <style jsx global>{`
         html,
         body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+          font-family: Roboto;
         }
         * {
           box-sizing: border-box;
