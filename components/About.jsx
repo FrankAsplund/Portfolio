4de332664ee88623777a82caa1c6e4d9d6a0f98d
FrankAsplund <frank_asplund@hotmail.com>
2023-06-16 18:10:08 +0200

    Working flip card (WIP)

--- components/About.jsx
+++ components/About.jsx
@@ -18,15 +18,30 @@ import React, { useEffect, useState } from "react";
 import ReactCardFlip from "react-card-flip";
 
 const About = () => {
-  const [isFlipped, setIsFlipped] = useState(false);
-
   useEffect(() => {
     AOS.init();
   }, []);
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    setIsFlipped(!isFlipped);
+  const [cardStates, setCardStates] = useState([
+    { isFlipped: false },
+    { isFlipped: false },
+    { isFlipped: false },
+    { isFlipped: false },
+    { isFlipped: false },
+  ]);
+
+  const handleClick = (index) => {
+    setCardStates((prevState) => {
+      const newState = [
+        { isFlipped: prevState[0].isFlipped },
+        { isFlipped: prevState[1].isFlipped },
+        { isFlipped: prevState[2].isFlipped },
+        { isFlipped: prevState[3].isFlipped },
+        { isFlipped: prevState[4].isFlipped },
+      ];
+      newState[index].isFlipped = !newState[index].isFlipped;
+      return newState;
+    });
   };
 
   return (
@@ -56,11 +71,14 @@ const About = () => {
           justifyContent="center"
           alignItems="center"
         >
-          <Grid item spacing={4} p={2} m={2} sx={{}}>
-            <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+          <Grid item spacing={4} p={2} m={2}>
+            <ReactCardFlip
+              isFlipped={cardStates[0].isFlipped}
+              flipDirection="horizontal"
+            >
               <div>
                 <Paper
-                  onClick={handleClick}
+                  onClick={() => handleClick(0)}
                   elevation={24}
                   sx={{
                     p: 8,
@@ -79,7 +97,7 @@ const About = () => {
 
               <div>
                 <Paper
-                  onClick={handleClick}
+                  onClick={() => handleClick(0)}
                   elevation={24}
                   sx={{
                     width: 278,
@@ -108,71 +126,218 @@ const About = () => {
           </Grid>
 
           <Grid item spacing={4} p={2} m={2}>
-            <Paper
-              elevation={24}
-              sx={{
-                p: 8,
-                mt: 8,
-              }}
+            <ReactCardFlip
+              isFlipped={cardStates[1].isFlipped}
+              flipDirection="horizontal"
             >
-              <Avatar
-                variant="rounded"
-                src="/images/home-cooked-meal.png"
-                sx={{ width: 150, height: 150 }}
-                alt="Pic"
-              />
-            </Paper>
+              <div>
+                <Paper
+                  onClick={() => handleClick(1)}
+                  elevation={24}
+                  sx={{
+                    p: 8,
+                    width: 278,
+                    height: 278,
+                  }}
+                >
+                  <Avatar
+                    variant="rounded"
+                    src="/images/home-cooked-meal.png"
+                    sx={{ width: 150, height: 150 }}
+                    alt="Pic"
+                  />
+                </Paper>
+              </div>
+
+              <div>
+                <Paper
+                  onClick={() => handleClick(1)}
+                  elevation={24}
+                  sx={{
+                    width: 278,
+                    height: 278,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    sx={{
+                      width: 150,
+                      height: 150,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    I like to cook meals at home
+                  </Typography>
+                </Paper>
+              </div>
+            </ReactCardFlip>
           </Grid>
 
           <Grid item spacing={4} p={2} m={2}>
-            <Paper
-              elevation={24}
-              sx={{
-                p: 8,
-                mt: 8,
-              }}
+            <ReactCardFlip
+              isFlipped={cardStates[2].isFlipped}
+              flipDirection="horizontal"
             >
-              <Avatar
-                variant="rounded"
-                src="/images/data.png"
-                sx={{ width: 150, height: 150 }}
-                alt="Pic"
-              />
-            </Paper>
+              <div>
+                <Paper
+                  onClick={() => handleClick(2)}
+                  elevation={24}
+                  sx={{
+                    p: 8,
+                    width: 278,
+                    height: 278,
+                  }}
+                >
+                  <Avatar
+                    variant="rounded"
+                    src="/images/data.png"
+                    sx={{ width: 150, height: 150 }}
+                    alt="Pic"
+                  />
+                </Paper>
+              </div>
+
+              <div>
+                <Paper
+                  onClick={() => handleClick(2)}
+                  elevation={24}
+                  sx={{
+                    width: 278,
+                    height: 278,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    sx={{
+                      width: 150,
+                      height: 150,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    I like to code
+                  </Typography>
+                </Paper>
+              </div>
+            </ReactCardFlip>
+          </Grid>
+          <Grid item spacing={4} p={2} m={2}>
+            <ReactCardFlip
+              isFlipped={cardStates[3].isFlipped}
+              flipDirection="horizontal"
+            >
+              <div>
+                <Paper
+                  onClick={() => handleClick(3)}
+                  elevation={24}
+                  sx={{
+                    p: 8,
+                    width: 278,
+                    height: 278,
+                  }}
+                >
+                  <Avatar
+                    variant="rounded"
+                    src="/images/gym.png"
+                    sx={{ width: 150, height: 150 }}
+                    alt="Pic"
+                  />
+                </Paper>
+              </div>
+
+              <div>
+                <Paper
+                  onClick={() => handleClick(3)}
+                  elevation={24}
+                  sx={{
+                    width: 278,
+                    height: 278,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    sx={{
+                      width: 150,
+                      height: 150,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    I like to work out
+                  </Typography>
+                </Paper>
+              </div>
+            </ReactCardFlip>
           </Grid>
 
           <Grid item spacing={4} p={2} m={2}>
-            <Paper
-              elevation={24}
-              sx={{
-                p: 8,
-                mt: 8,
-              }}
+            <ReactCardFlip
+              isFlipped={cardStates[4].isFlipped}
+              flipDirection="horizontal"
             >
-              <Avatar
-                variant="rounded"
-                src="/images/gym.png"
-                sx={{ width: 150, height: 150 }}
-                alt="Pic"
-              />
-            </Paper>
-          </Grid>
+              <div>
+                <Paper
+                  onClick={() => handleClick(4)}
+                  elevation={24}
+                  sx={{
+                    p: 8,
+                    width: 278,
+                    height: 278,
+                  }}
+                >
+                  <Avatar
+                    variant="rounded"
+                    src="/images/cat.png"
+                    sx={{ width: 150, height: 150 }}
+                    alt="Pic"
+                  />
+                </Paper>
+              </div>
 
-          <Grid item spacing={4} p={2} m={2}>
-            <Paper
-              elevation={24}
-              sx={{
-                p: 8,
-                mt: 8,
-              }}
-            >
-              <Avatar
-                variant="rounded"
-                src="/images/cat.png"
-                sx={{ width: 150, height: 150 }}
-                alt="Pic"
-              />
-            </Paper>
+              <div>
+                <Paper
+                  onClick={() => handleClick(4)}
+                  elevation={24}
+                  sx={{
+                    width: 278,
+                    height: 278,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    sx={{
+                      width: 150,
+                      height: 150,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    I like to play with my cats
+                  </Typography>
+                </Paper>
+              </div>
+            </ReactCardFlip>
           </Grid>
         </Grid>
       </Container>
@@ -191,200 +356,6 @@ const About = () => {
 };
 
 export default About;
-
-/* export default function About() {
-  useEffect(() => {
-    AOS.init();
-  }, []);
-
-  const [flipped, setFlipped] = useState(false);
-
-  const handleFlip = () => {
-    setFlipped(!flipped);
-  };
-  return ( */
-/* <main data-aos="zoom-in-up" data-aos-duration="4000">
-      <Container
-        sx={{
-          display: "grid",
-          mt: "6rem",
-          mb: "4rem",
-          justifyItems: "center",
-          flexWrap: "wrap",
-        }}
-      >
-        <Typography gutterBottom variant="h3">
-          Hi!
-        </Typography>
-        <Typography gutterBottom variant="h5">
-          This is what I like to do on my free time.
-        </Typography>
-
-        <Grid
-          container
-          rowSpacing={1}
-          spacing={0}
-          minHeight={160}
-          columns={2}
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Grid
-            item
-            spacing={4}
-            p={2}
-            m={2}
-            style={{
-              width: 250,
-              height: 250,
-              perspective: "1000px",
-              margin: "0 auto",
-              transformStyle: "preserve-3d",
-              transform: `rotateY(${flipped ? 180 : 0}deg)`,
-              transition: "transform 0.5s",
-            }}
-          >
-            <Paper
-              elevation={24}
-              style={{
-                backfaceVisibility: "hidden",
-              }}
-              sx={{
-                p: 8,
-                mt: 8,
-              }}
-              onClick={handleFlip}
-            >
-              {flipped ? (
-                <div>
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    sx={{ textAlign: "center", transform: "rotateY(0deg)" }}
-                  >
-                    I like to play guitar
-                  </Typography>
-                </div>
-              ) : (
-                <div
-                  style={{
-                    backfaceVisibility: "hidden",
-                  }}
-                >
-                  <Avatar
-                    variant="rounded"
-                    src="/images/guitar.png"
-                    sx={{ width: 150, height: 150 }}
-                    alt="Pic"
-                  />
-                </div>
-              )}
-            </Paper>
-          </Grid>
-
-          <Grid item spacing={4} p={2} m={2}>
-            <Paper
-              elevation={24}
-              sx={{
-                p: 8,
-                mt: 8,
-              }}
-            >
-              <Avatar
-                variant="rounded"
-                src="/images/home-cooked-meal.png"
-                sx={{ width: 150, height: 150 }}
-                alt="Pic"
-              />
-            </Paper>
-          </Grid>
-
-          <Grid item spacing={4} p={2} m={2}>
-            <Paper
-              elevation={24}
-              sx={{
-                p: 8,
-                mt: 8,
-              }}
-            >
-              <Avatar
-                variant="rounded"
-                src="/images/data.png"
-                sx={{ width: 150, height: 150 }}
-                alt="Pic"
-              />
-            </Paper>
-          </Grid>
-
-          <Grid item spacing={4} p={2} m={2}>
-            <Paper
-              elevation={24}
-              sx={{
-                p: 8,
-                mt: 8,
-              }}
-            >
-              <Avatar
-                variant="rounded"
-                src="/images/gym.png"
-                sx={{ width: 150, height: 150 }}
-                alt="Pic"
-              />
-            </Paper>
-          </Grid>
-
-          <Grid item spacing={4} p={2} m={2}>
-            <Paper
-              elevation={24}
-              sx={{
-                p: 8,
-                mt: 8,
-              }}
-            >
-              <Avatar
-                variant="rounded"
-                src="/images/cat.png"
-                sx={{ width: 150, height: 150 }}
-                alt="Pic"
-              />
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
-      <style jsx>{`
-        main {
-          font-family: Roboto;
-          padding: 2rem 0;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-      `}</style>
-    </main>
-  );
-} */
-
-/* import { useEffect } from "react";
-import ReactFlipCard from "reactjs-flip-card";
-
-export default function About() {
-  return (
-    <main>
-
-                  <Avatar
-                    variant="rounded"
-                    src="/images/guitar.png"
-                    sx={{ width: 150, height: 150 }}
-                    alt="Pic"
-                  />
-                  <Typography gutterBottom variant="h5">
-                    I like to play guitar
-                  </Typography>
-              />
-    </main>
-  );
-} */
 
 /* const aboutData = [
   {
